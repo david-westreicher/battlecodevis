7ec8b6be356f9e595f966d8ffe466fab66a62d47
Juan Pablo <juanolon@gmail.com>
2015-01-15 03:55:28 +0100

    WIP update

---
+++ main.js
@@ -0,0 +1,47 @@
+var gzip = require('gzip-js');
+//     options = {
+//         level: 3,
+//         name: 'hello-world.txt',
+//         timestamp: parseInt(Date.now() / 1000, 10)
+//     };
+//
+// // out will be a JavaScript Array of bytes
+// var out = gzip.zip('Hello world', options);
+
+var ChunkReader = function(file){
+    var self = this;
+    self.update = function(){
+        console.log(self.start);
+        console.log(self.end);
+        console.log(self.file);
+    }
+    self.readNextLine = function(){
+        if(self.start<self.file.size)
+            self.reader.readAsBinaryString(self.file.slice(self.start,self.end));
+        else{
+            console.log("EOF")
+            console.log("time: "+(new Date().getTime()-self.startTime.getTime()));
+        }
+        self.start+=self.chunkSize;
+        self.end+=self.chunkSize;
+        self.end= Math.min(self.file.size,self.end);
+    }
+    self.startTime = new Date();
+    self.chunkSize = 1024;
+    self.chunkSize = 2048;
+    self.chunkSize = 4096;
+    self.start = 0;
+    self.end = self.chunkSize;
+    self.reader = new FileReader();
+    self.reader.onload = function(e){
+        // console.log(self.reader.result);
+        self.readNextLine();
+    }
+    self.file = file;
+}
+window.fileSelected = function(file){
+    console.log("test");
+    var out = gzip.unzip(file.getAsBinary());
+    console.log(out);
+    new ChunkReader(out).readNextLine()
+};
