807df90f9f063c3f769f64c85e77c47021704622
David Westreicher <david.westreicher@gmail.com>
2015-01-19 08:36:47 +0100

    teamnames are now accessible

--- teams/extractteams.py
+++ teams/extractteams.py
@@ -2,12 +2,14 @@
 teamLink = "http://www.battlecode.org/contestants/teams/"
 f = open('Battlecode - AI Programming Competition.html', 'r')
 teams = []
+lineCount = 0
 for line in f:
-    if teamLink in line:
+    lineCount+=1
+    if lineCount>2 and teamLink in line:
         numindex = line.find(teamLink)+len(teamLink)
         numstart = line[numindex:]
         teamNum = numstart[:numstart.find('"')]
-        teamName = numstart[numstart.find('>')+1:numstart.find('<')]
+        teamName = numstart[numstart.find('>')+1:numstart.find('<')].translate(None, '"')
         teams.append([teamNum,teamName])
 f.close()
 print(teams)
@@ -15,5 +17,5 @@ print(teams)
 f = open('teams.js', 'w')
 f.write('var teams = {};\n')
 for team in teams:
-    f.write('team['+team[0]+'] = "'+team[1]+'";\n')
+    f.write('teams['+team[0]+'] = "'+team[1]+'";\n')
 f.close()
