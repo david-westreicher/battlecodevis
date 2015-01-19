0d3234b31c37c09d86e71de5136c92a3b51b4fa3
David Westreicher <david.westreicher@gmail.com>
2015-01-19 19:55:19 +0100

    add team images

--- teams/extractteams.py
+++ teams/extractteams.py
@@ -3,19 +3,23 @@ teamLink = "http://www.battlecode.org/contestants/teams/"
 f = open('Battlecode - AI Programming Competition.html', 'r')
 teams = []
 lineCount = 0
+currentAvatar = None
 for line in f:
     lineCount+=1
+    if 'avatar' in line:
+        currentAvatar = line[line.find("files/")+len('files/'):line.find('png')+3]
     if lineCount>2 and teamLink in line:
         numindex = line.find(teamLink)+len(teamLink)
         numstart = line[numindex:]
         teamNum = numstart[:numstart.find('"')]
         teamName = numstart[numstart.find('>')+1:numstart.find('<')].translate(None, '"')
-        teams.append([teamNum,teamName])
+        teams.append([teamNum,teamName,currentAvatar])
 f.close()
-print(teams)
+for team in teams:
+    print(team)
 
 f = open('teams.js', 'w')
 f.write('var teams = {};\n')
 for team in teams:
-    f.write('teams['+team[0]+'] = "'+team[1]+'";\n')
+    f.write('teams['+team[0]+'] = ["'+team[1]+'","'+team[2]+'"];\n')
 f.close()
