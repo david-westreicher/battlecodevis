f9ff0f971f79ba73dfc9f36f61917145e92708ff
David Westreicher <david.westreicher@gmail.com>
2015-01-20 06:24:49 +0100

    test modelrenderer performance, add new models

--- camera.js
+++ camera.js
@@ -1,7 +1,7 @@
 var battlecodeCamera = function(){
 	var self = this;
-	self.cameraDist = 2000;
-	self.cam = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 1, 10000 );
+	self.cameraDist = 200;
+	self.cam = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 1, 1000 );
 
 	self.updateRatio = function(){
 		self.cam.aspect = window.innerWidth / window.innerHeight;
@@ -12,7 +12,7 @@ var battlecodeCamera = function(){
 			self.cameraDist*=1.1;
 		}else
 			self.cameraDist/=1.1;
-		self.cameraDist = Math.min(5000,Math.max(100,self.cameraDist));
+		self.cameraDist = Math.min(500,Math.max(10,self.cameraDist));
 	}
 	self.update = function(angle1,angle2,pos){
 		var cameraRadius = Math.abs(Math.sin(angle2))*self.cameraDist;
--- modelrenderer.js
+++ modelrenderer.js
@@ -1,50 +1,86 @@
 var ModelRenderer = function(){
 	var self = this;
-	self.models = ["models/Suzanne.js","models/monster.js"];
 	self.types = {
-		"DRONE":0,
-		"HQ":1,
-		"BEAVER":1,
-		"TOWER":1,
-		"TANK":1,
+		"AEROSPACELAB":{model:"models/box.js"},
+		"BARRACKS":{model:"models/box.js"},
+		"BEAVER":{model:"models/Suzanne.js"},
+		"COMMANDER":{model:"models/Suzanne.js"}, 
+		"COMPUTER":{model:"models/box.js"},
+		"DRONE":{model:"models/drone.js",height:25,shootHeight:25},
+		"HANDWASHSTATION":{model:"models/box.js"},
+		"HELIPAD":{model:"models/box.js"},
+		"HQ":{model:"models/hq.js",shootHeight:40},
+		"LAUNCHER":{model:"models/Suzanne.js"},
+		"MINER":{model:"models/Suzanne.js"},
+		"MINERFACTORY":{model:"models/box.js"},
+		"MISSILE":{model:"models/Suzanne.js"},
+		"SOLDIER":{model:"models/Suzanne.js"},
+		"SUPPLYDEPOT":{model:"models/box.js"},
+		"TANK":{model:"models/Suzanne.js"},
+		"TANKFACTORY":{model:"models/box.js"},
+		"TECHNOLOGYINSTITUTE":{model:"models/box.js"},
+		"TOWER":{model:"models/tower.js",shootHeight:22},
+		"TRAININGFIELD":{model:"models/box.js"},
 	};
 	self.geometries = [];
-	self.loadedModels = 0;
 	self.meshes = [];
 
 	self.loadModel = function(loader){
-		if(self.models.length<=self.loadedModels)
+		if(self.models.length<=self.geometries.length)
 			return;
-		var model = self.models[self.loadedModels];
+		var model = self.models[self.geometries.length];
 		console.log("loading "+model+"!");
 		loader.load(model,function(geometry){
-			console.log(self.models[self.loadedModels]+" loaded!");
-			geometry.center();
-			geometry.computeVertexNormals();
+			console.log(self.models[self.geometries.length]+" loaded!");
+			//geometry.computeVertexNormals();
+			for(var type in self.types){
+			    var currentType = self.types[type]
+	            if(self.models[self.geometries.length]==currentType.model)
+                    currentType.modelID = self.geometries.length;
+	        }
+			self.meshes.push([]);
 			self.geometries.push(geometry);
-			self.loadedModels++;
 			self.loadModel(loader);
 		});
 	}
+
+	self.createModelsArray = function(){
+	    self.models = [];
+	    for(var type in self.types){
+	        var model = self.types[type].model;
+	        var isAlreadyHere = false;
+	        for(var i=0;i<self.models.length;i++){
+	            if(self.models[i]==model){
+	                isAlreadyHere = true;
+	                break;
+	            }
+	        }
+	        if(!isAlreadyHere)
+	            self.models.push(model);
+	    }
+	    console.log(self.models);
+	}
+
 	self.init = function(){
-		for(var i=0;i<self.models.length;i++){
-			self.meshes.push([]);
-		}
+	    self.meshes = [];
 		var loader = new THREE.JSONLoader();
 		self.normalMaterial = new THREE.MeshLambertMaterial( { color: 0xffffff } );
         self.redMaterial = new THREE.MeshLambertMaterial( { color: 0xff0000 } );
 	    self.blueMaterial = new THREE.MeshLambertMaterial( { color: 0x0000ff } );
+	    self.createModelsArray();
 		self.loadModel(loader);
 	}
 
 	self.createMesh = function(geometry){
 		var mesh = new THREE.Mesh( geometry, self.normalMaterial );
+		mesh.rotation.x = Math.PI/2;
+		mesh.scale.x = mesh.scale.y = mesh.scale.z = 5; 
 		mesh.castShadow = true;
 		return mesh;
 	}
 
 	self.draw = function(scene,simData){
-		if(self.models.length>self.loadedModels)
+		if(self.models.length>self.geometries.length)
 			return;
 		var meshCounter = [];
 		for(var i=0;i<self.models.length;i++){
@@ -52,27 +88,22 @@ var ModelRenderer = function(){
 		}
 		for(var id in simData.robots){
 			var robot = simData.robots[id];
-			var needMesh = self.types[robot.type];
-			if(!needMesh)
+			var type = self.types[robot.type];
+			var modelID = type.modelID;
+			if(!modelID)
 				//default mesh
-				needMesh = 0;
-			meshCounter[needMesh]++;
+				modelID = 0;
+			meshCounter[modelID]++;
 			var mesh = null;
-			if(meshCounter[needMesh]>self.meshes[needMesh].length){
-				mesh = self.createMesh(self.geometries[needMesh]);
+			if(meshCounter[modelID]>self.meshes[modelID].length){
+				mesh = self.createMesh(self.geometries[modelID]);
 				scene.add( mesh );
-				self.meshes[needMesh].push(mesh);
+				self.meshes[modelID].push(mesh);
 			}else{
-				mesh = self.meshes[needMesh][meshCounter[needMesh]-1];
+				mesh = self.meshes[modelID][meshCounter[modelID]-1];
 			}
 			//update position ....
 	        var realPos = getInterpPosition(robot);
-	        if(needMesh==1)
-		        mesh.scale.x = mesh.scale.y = mesh.scale.z = (robot.type=='TOWER'?0.10:0.05);
-		    else
-		        mesh.scale.x = mesh.scale.y = mesh.scale.z = (robot.type=='TOWER'?60:30);
-		    if(robot.type=='TOWER')
-			    mesh.scale.z*=0.3;
 		    var yDif = robot.lastloc[1]-robot.loc[1];
 		    var xDif = robot.lastloc[0]-robot.loc[0];
 		    var angleDiff = -Math.atan2(yDif,xDif)+Math.PI-robot.rot;
@@ -86,7 +117,7 @@ var ModelRenderer = function(){
 		    mesh.rotation.y = robot.rot;
 		    mesh.position.x = realPos[0];
 		    mesh.position.y = realPos[1];
-		    mesh.position.z = robot.type!='DRONE'?40:200;
+		    mesh.position.z = type.height?type.height:0;
 		    mesh.material = (robot.team=='A')?self.redMaterial:self.blueMaterial;
 		}
 		//remove unnecessary meshes if meshCounter[i]<self.meshes[i]
