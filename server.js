893ba07998c13023b98b8027e8a482e180fa0493
David Westreicher <david.westreicher@gmail.com>
2015-01-20 21:46:18 +0100

    fix heroku #3

--- server.js
+++ server.js
@@ -5,9 +5,14 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+    response.sendFile('index.html', {"root": __dirname});
+});
+
+app.get(/^(.+)$/, function(req, res){ 
+    console.log(req.params[0]);
+    res.sendFile(req.params[0], {"root": __dirname}); 
 });
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+    console.log("Node app is running at localhost:" + app.get('port'));
 });
